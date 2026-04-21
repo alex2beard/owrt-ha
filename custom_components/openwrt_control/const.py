55e48a59101fc8545bca050fdf0ffa1411c08773
alex2beard <77844696+alex2beard@users.noreply.github.com>
2026-04-21 20:47:51 +0300

    Remove hardcoded connection defaults

--- custom_components/openwrt_control/const.py
+++ custom_components/openwrt_control/const.py
@@ -4,10 +4,6 @@ from homeassistant.const import Platform
 
 DOMAIN = "openwrt_control"
 
-DEFAULT_HOST = "owrt.frolkin.com"
-DEFAULT_PORT = 443
-DEFAULT_USE_HTTPS = True
-DEFAULT_VERIFY_SSL = True
 DEFAULT_SCAN_INTERVAL = 30
 DEFAULT_REQUEST_TIMEOUT = 10
 DEFAULT_SESSION_TIMEOUT = 300
