d295885ee8071d730ce7b9bf224d036f5eb705c3
alex2beard <77844696+alex2beard@users.noreply.github.com>
2026-04-21 21:33:19 +0300

    Add configurable OpenConnect interface defaults

--- custom_components/openwrt_control/const.py
+++ custom_components/openwrt_control/const.py
@@ -4,10 +4,16 @@ from homeassistant.const import Platform
 
 DOMAIN = "openwrt_control"
 
+DEFAULT_HOST = ""
+DEFAULT_PORT = 443
+DEFAULT_USE_HTTPS = True
+DEFAULT_VERIFY_SSL = True
 DEFAULT_SCAN_INTERVAL = 30
 DEFAULT_REQUEST_TIMEOUT = 10
 DEFAULT_SESSION_TIMEOUT = 300
+DEFAULT_OPENCONNECT_INTERFACE = "vpn"
 
+CONF_OPENCONNECT_INTERFACE = "openconnect_interface"
 CONF_USE_HTTPS = "use_https"
 
 PLATFORMS: list[Platform] = [
